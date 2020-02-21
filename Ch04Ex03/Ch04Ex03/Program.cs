aaaa512fa4dd16bcef5eb58460fd323a1023f32d
abigaillealperez <lealperezabigail.alp@gmail.com>
2020-02-20 19:23:11 -0500

    Create Program.cs

---
+++ Ch04Ex03/Ch04Ex03/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch04Ex03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string myName = "abigail";
+            const string sexyName = "trace";
+            const string sillyName = "kristina";
+            string name;
+            Console.WriteLine("What is your name?");
+            name = Console.ReadLine();
+            switch (name.ToLower())
+            {
+                case myName:
+                    Console.WriteLine("You have the same name as me!");
+                break;
+                case sexyName:
+                    Console.WriteLine("My, What a SEXY NAME YOU HAVE!");
+                break;
+                case sillyName:
+                    Console.WriteLine("Thats a very silly and ugly name like you!");
+                break;
+                default:
+                    Console.WriteLine("Your name is weird, I have never heard it before.");
+                break;
+            }
+            Console.WriteLine("Hello {0}!", name);
+            Console.ReadKey();
+
+
+        }
+    }
+}
